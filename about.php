c7d526f8488ab3c6a333e529062156e627f94f76
Singularity112 <singularity123097@gmail.com>
2018-03-21 19:56:08 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,155 @@
+<?php require_once ('header-about.php'); ?>
+
+<link href = "css/about.css" rel = "stylesheet">
+
+<div class = "container-fluid back" id = "banner_main">
+    <div class = "breadcr">
+        <span><a href = "#">צור_קשר</a></span>
+        <span class = "res">/</span>
+        <span><a href = "#" class = "acitve">אודות טכנולייט</a></span>
+    </div>
+    <div class = "about-container">
+        <div class = "about-page-title">
+            אודות טכנוליט
+        </div>
+        <div class = "about-page-desc">
+            חברת "טכנולייט בע"מ" הינה אחת החברות הותיקות והמשפיעות בתחום התאורה בישראל. טכנולייט בעלת ניסיון רב בשוק התאורה ובייצור גופי תאורה אדריכלית, דקורטיביי, ותאורת חוץ.
+        </div>
+    </div>
+</div>
+
+<div class = "container-fluid back" id = "about_pic">
+    <div class = "about-container">
+        <div class = "about_pic-content">
+            <div class = "about-pic-title">
+                הסיפור שלנו
+            </div>
+            <div class = "about-pic-href">
+                חברת "טכנולייט בע"מ" הינה אחת החברות הותיקות והמשפיעות בתחום התאורה בישראל. טכנולייט בעלת ניסיון רב בשוק התאורה ובייצור גופי תאורה אדריכלית, דקורטיביי, ותאורת חוץ.
+
+
+            </div>
+            <div class = "about-pic-desc">
+                טכנולייט מספקת ללקוחותיה את המוצרים האיכותיים ביותר ומציעה מגוון רחב של פתרונות תאורה המותאמים לכל חלל, טעם וסגנון.<br>
+טכנולייט מייצרת ומתמחה בתאורת פנים, תאורת חוץ ומאווררי תקרה. שליטה ובקרה על כל תהליך הייצור, מבטיחה סטנדרט גבוה הנשמרת בכל טווח המוצרים.
+
+            </div>
+            <div class = "about-pic-desc">
+                ורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה. דס איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל אאוגו וסטיבולום סוליסי טידום בעליק. קולהע צופעט למרקוח איבן איף
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id = "parallax" class = "container-fluid back">
+    <div class = "about-container">
+        <div class = "parallax-content">
+            <div class = "parallax-title">
+                איתרונות של החברה
+            </div>
+            <div class = "parallax-images">
+                <div class = "image-item">
+                    <img src = "img/about-par1.png">
+                    <span>פריסה עולמים</span>
+                </div>
+                <div class = "image-item">
+                    <img src = "img/about-par2.png">
+                    <span>QC Driven Manufacturing</span>
+                </div>
+                <div class = "image-item">
+                    <img src = "img/about-par3.png">
+                    <span>טכנולוגיה מתקדמת</span>
+                </div>
+                <div class = "image-item">
+                    <img src = "img/about-par4.png">
+                    <span>עיצוב ופיתוח מוצרים חדשנים</span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class = "container-fluid back" id = "people">
+    <div class = "about-container">
+        <div class = "about_pic-content">
+            <div class = "about-pic-title">
+  אזור טקסט
+            </div>
+            <div class = "about-pic-href">
+חברת "טכנולייט בע"מ" הינה אחת החברות הותיקות והמשפיעות בתחום התאורה בישראל. טכנולייט בעלת ניסיון רב בשוק התאורה ובייצור גופי תאורה אדריכלית, דקורטיביי, ותאורת חוץ.
+
+            </div>
+            <div class = "about-pic-desc">
+                טכנולייט מספקת ללקוחותיה את המוצרים האיכותיים ביותר ומציעה מגוון רחב של פתרונות תאורה המותאמים לכל חלל, טעם וסגנון.<Br>
+טכנולייט מייצרת ומתמחה בתאורת פנים, תאורת חוץ ומאווררי תקרה. שליטה ובקרה על כל תהליך הייצור, מבטיחה סטנדרט גבוה הנשמרת בכל טווח המוצרים.
+
+            </div>
+            <div class = "about-pic-desc">
+ורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית. סת אלמנקום ניסי נון ניבאה. דס איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל אאוגו וסטיבולום סוליסי טידום בעליק. קולהע צופעט למרקוח איבן איף            </div>
+        </div>
+    </div>
+</div>
+
+<div id = "works" class = "container-fluid">
+    <div class = "about-container">
+        <div class = "works-content">
+            <div class = "works-title">
+                המותגים שלנו
+            </div>
+            <div class = "works-desc">
+                לורם יפסום דולור סיט אמת לורם יפסום דולור סיט אמת
+
+            </div>
+
+            <div class = "works-items">
+                <div class = "works-item">
+                    <img src = "img/Technolite_logo.png">
+                    <a href = "#">שם המותג</a>
+                    <div class = "works-item-desc">
+                        לורם יפסום דולור סיט אמת לורם יפסום דולור סיט אמת
+
+                    </div>
+                </div>
+
+                <div class = "works-item">
+                    <img src = "img/mumoon-logo.png">
+                    <a href = "#">שם המותג</a>
+                    <div class = "works-item-desc">
+                        לורם יפסום דולור סיט אמת לורם יפסום דולור סיט אמת
+
+                    </div>
+                </div>
+
+                <div class = "works-item">
+                    <img src = "img/Technolite_logo.png">
+                    <a href = "#">שם המותג</a>
+                    <div class = "works-item-desc">
+                        לורם יפסום דולור סיט אמת לורם יפסום דולור סיט אמת
+
+                    </div>
+                </div>
+
+                <div class = "works-item">
+                    <img src = "img/Swingfans.png">
+                    <a href = "#">שם המותג</a>
+                    <div class = "works-item-desc">
+                        לורם יפסום דולור סיט אמת לורם יפסום דולור סיט אמת
+
+                    </div>
+                </div>
+
+                <div class = "works-item">
+                    <img src = "img/Technolite_logo.png">
+                    <a href = "#">שם המותג</a>
+                    <div class = "works-item-desc">
+                        לורם יפסום דולור סיט אמת לורם יפסום דולור סיט אמת
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once ('footer.php')?>
---
+++ header-about.php
@@ -0,0 +1,81 @@
+<meta charset="UTF-8">
+<link rel = "stylesheet" href = "css/header-about.css">
+<div id = "topline">
+    <div class = "topline-item">
+        03-556-8433 <img src = "img/phone-topline.png">
+    </div>
+    <div class = "topline-item">
+        ייעוץ  עם מומחה <img src = "img/question-topline.png">
+    </div>
+    <div class = "topline-item">
+        התחברות <img src = "img/user-topline.png">
+    </div>
+</div>
+
+<div id = "navigation">
+    <div class = "nav-items">
+        <div class = "nav-item">
+<a href = "#">הסיפור_שלנו</a>
+        </div>
+        <div class = "nav-item">
+<a href = "#">
+    מוצרים
+</a>        </div>
+        <div class = "nav-item">
+<a href = "#">פרוייקטים</a>
+        </div>
+        <div class = "nav-item">
+<a href = "#">בלוג</a>
+        </div>
+        <div class = "nav-item">
+            <a href = "#">צור_קשר</a>
+        </div>
+        <div class = "nav-item">
+<img src = "img/about-header-search.png">
+        </div>
+    </div>
+    <div class = "logo">
+        <img src = "img/logo.png">
+    </div>
+</div>
+
+<div id = "mobile-hamburger">
+    <div class = "mobile-logo">
+        <img src = "img/logo.png">
+    </div>
+
+    <div id = "mobile-opener">
+         <i id = "anim" class = "fa fa-bars"></i>
+    </div>
+</div>
+
+<div id = "mobile-menu">
+    <div class = "mobile-menu-item">
+        <a href = "#">הסיפור_שלנו</a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#">מוצרים</a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#">פרוייקטים</a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#">בלוג</a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#">צור_קשר</a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#"><i class = "fa fa-search"></i></a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#">03-556-8433 <img src = "img/phone-topline.png"></a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#">ייעוץ  עם מומחה<img src = "img/question-topline.png"></a>
+    </div>
+    <div class = "mobile-menu-item">
+        <a href = "#">התחברות<img src = "img/user-topline.png"></a>
+    </div>
+</div>
+<script src = "js/mobile-menu.js"></script>
