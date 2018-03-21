12ce7c53ed8a948f2ebe424eeaec1b5e892b18e0
hikaram <hikaram@gmail.com>
2018-03-21 18:08:21 +0200

    project page

--- project.php
+++ project.php
@@ -5,20 +5,9 @@ get_header("front-page"); ?>
 	
 </section>
 
-
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="pull-right">
-				<h2 class="title-box">פרוייקט וילה מארה</h2>
-			</div>
-		</div>
-	</div>
-</div>
-
 <section class="post container">
 	<article>
-		<h1></h1>
+		<h1>פרוייקט וילה מארה</h1>
 		<div class="row share">
 			<div class="col-md-6">
 						<ul class="list-date">
@@ -37,7 +26,7 @@ get_header("front-page"); ?>
 		</div>
 		<h4>לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית נולום ארווס סאפיאן - פוסיליס קוויס, אקווזמן קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון בלרק - וענוף לפרומי בלוף קינץ תתיח לרעח. לת צשחמי קולהע צופעט למרקוח איב</h4>
 		<p>
-			לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית נולום ארווס סאפיאן - פוסיליס קוויס, אקווזמן קוואזי במר מודוף וענוף לפרומי בלוף קינץ תתיח לרעח. לת צשחמי קולהע צופ	
+			<a href="#">לורם איפסום דולור סיט אמט</a>, קונסקטורר אדיפיסינג אלית נולום ארווס סאפיאן - פוסיליס קוויס, אקווזמן קוואזי במר מודוף וענוף לפרומי בלוף קינץ תתיח לרעח. לת צשחמי קולהע צופ	
 		</p>
 		<p>
 			למי שתהיה סבלנות להמשיך ולקרוא, בסוף המאמר אני אחשוף פרויקט חם מהתנור של אתר שגם קודם בתקציב נמוך, גם נהנה מפניות וממיקומים ראשונים בביטוים התחרותיים ביותר בתחום שלו, וגם מרוצה – קיצור שווה לחכות
@@ -45,19 +34,69 @@ get_header("front-page"); ?>
 		<h4>קונסקטורר אדיפיסינג אלית</h4>
 		<p>קולורס מונפרד אדנדום סילקוף, מרגשי ומרגשח. עמחליף סחטיר בלובק. תצטנפל בלינדו למרקל אס לכימפו, דול, צוט ומעיוט - לפתיעם ברשג - ולתיעם גדדיש. קוויז דומור ליאמום בלינך רוגצה. לפמעט מוסן מנת</p>
 		<p>קולורס מונפרד אדנדום סילקוף, מרגשי ומרגשח. עמחליף סחטיר בלובק. תצטנפל בלינדו למרקל אס לכימפו, דול, צוט ומעיוט - לפתיעם ברשג - ולתיעם גדדיש. קוויז דומור ליאמום בלינך רוגצה. לפמעט מוסן מנת</p>
-		<p>סחטיר בלובק. תצטנפל בלינדו למרקל אס לכימפו, דול, צוט ומעיוט - לפתיעם ברשג - ולתיעם גדדיש. קוויז דומור ליאמום בלינך רוגצה. לפמעט מוסן מנת. צש בליא, מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק</p>
+		<p class="blue-text">סחטיר בלובק. תצטנפל בלינדו למרקל אס לכימפו, דול, צוט ומעיוט - לפתיעם ברשג - ולתיעם גדדיש. קוויז דומור ליאמום בלינך רוגצה. לפמעט מוסן מנת. צש בליא, מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק</p>
 		<h3>מוצרים משולבים בפרוייקט:</h3>
-		<ul>
-			<li><img src="../img/project-mini.png" alt="" /></li>
-			<li><img src="../img/project-mini.png" alt="" /></li>
-			<li><img src="../img/project-mini.png" alt="" /></li>
-		</ul>
+
+		<table class="miniproject-list">
+			<tr>
+				<td><img src="../img/project-mini.png" alt="" /></td>				
+				<td>
+					<h3>פרולייט P50</h3>
+					<ul>
+						<li>White, Black</li>
+						<li>IP20</li>
+						<li>220-240V</li>
+						<li>HEP</li>
+						<li>3000K</li>
+						<li>CRI>80</li>
+						<li>530Lm</li>
+						<li></li>
+					</ul>
+				</td>
+				<td class="miniproject-list_price"> 790 ש"ח</td>				
+			</tr>
+			<tr>
+				<td><img src="../img/project-mini.png" alt="" /></td>				
+				<td>
+					<h3>פרולייט P50</h3>
+					<ul>
+						<li>White, Black</li>
+						<li>IP20</li>
+						<li>220-240V</li>
+						<li>HEP</li>
+						<li>3000K</li>
+						<li>CRI>80</li>
+						<li>530Lm</li>
+						<li></li>
+					</ul>
+				</td>
+				<td class="miniproject-list_price"> 790 ש"ח</td>				
+			</tr>
+			<tr>
+				<td><img src="../img/project-mini.png" alt="" /></td>				
+				<td>
+					<h3>פרולייט P50</h3>
+					<ul>
+						<li>White, Black</li>
+						<li>IP20</li>
+						<li>220-240V</li>
+						<li>HEP</li>
+						<li>3000K</li>
+						<li>CRI>80</li>
+						<li>530Lm</li>
+						<li></li>
+					</ul>
+				</td>
+				<td class="miniproject-list_price"> 790 ש"ח</td>				
+			</tr>						
+		</table>
+
 		<div class="row">
-			<div class="col-md-6">
-				<a href="#" class="post-prev">פרויקט הבא</a>
+			<div class="col-md-6" style="text-align: right">
+				<a href="#" class="post-prev">פרויקט הבא <span class="lnr lnr-chevron-right"></span></a>
 			</div>
-			<div class="col-md-6">
-				<a href="#" class="post-next">פרוייקט קודם</a>
+			<div class="col-md-6" style="text-align: left">
+				<a href="#" class="post-next"><span class="lnr lnr-chevron-left"></span> פרוייקט קודם</a>
 			</div>
 		</div>
 	</article>
