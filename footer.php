67c565274eaf27322d4ef49b01c22d4b9380b9ef
Gavrilovich Alexander <gavrilovich.a@gmail.com>
2018-03-21 22:29:37 +0200

    end footer

--- footer.php
+++ footer.php
@@ -1,40 +1,17 @@
-<!DOCTYPE html>
-<html dir="rtl" lang="he-IL">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
-    <title>GEO - front page</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <!-- Custom styles for this template -->
-<!--    <link rel="stylesheet" href="css/owl.carousel.min.css">-->
-<!--    <link rel="stylesheet" href="css/owl.theme.default.min.css">-->
-    <link rel="stylesheet" href="css/linearicons-free.min.css">
-    <link rel="stylesheet" href="css/opensanshebrew.min.css">
-    <link rel="stylesheet" href="css/fontello.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/footer.min.css">
-    <link rel="stylesheet" href="css/front-page.css">
-    <link rel="stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-<!--    <link rel="stylesheet" href="css/front-page-mobile.css">-->
-<!--    <link rel ="stylesheet" href = "css/mobile.css">-->
-</head>
-
-<body>
-	<footer class="main-footer">
+<footer class="main-footer">
+    <div class="footer-top_line">
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                     <div class="footer-column column-logo">
                         <img src="img/Technolite_footer.png" alt="logo" class="logo-footer">
                         <h3>הרשמה לניוזלטר</h3>
-                        <p>דוא”ל</p>
+
+                        <form action="" class="footer-form">
+                            <input type="text" name="textForm" placeholder="דוא”ל">
+                            <button type="submit" name="submit" class="btn-form"></button>
+                        </form>
+
                         <div class="footer-social">
                             <nav class="soc">
                                 <ul>
@@ -44,11 +21,6 @@
                                 </ul>
                             </nav>
                         </div>
-                        <div class="link-left">
-                            <ul>
-                                <li><a href=""></a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
 
@@ -56,9 +28,9 @@
                     <div class="footer-column column-contacts">
                         <a href="#" class="link-title">דרכי התקשרות</a>
                         <div class="bottom-link_wrap">
-                            <p><strong>טtל:</strong> 03-556-8433</p>
+                            <p><strong>פקס:</strong> 03-556-8433</p>
                             <p><strong>פקס:</strong> 03-558-1420</p>
-                            <p><strong>מייל:</strong> Info@tlite.co.il</p>
+                            <p><strong>מייל:</strong><a href="#" class="link-email">Info@tlite.co.il</a></p>
                             <p><strong>כתובת:</strong> ת.ד. 172, היהלום 1</p>
                             <p>ברקת, ישראל </p>
                         </div>
@@ -106,40 +78,36 @@
                 </div>
             </div>
         </div>
-	</footer>
+    </div>
 
     <div class="footer-bottom_line">
         <div class="container">
             <div class="row">
                 <div class="col-md-4 col-sm-6">
-                    <div class="footer-column"><p> © 2018  כל הזכויות שמורות לחברת טכנולייט</p></div>
-                </div>
-
-                <div class="col-md-4 col-sm-6">
                     <div class="footer-column">
-                        <div class="breadcrumbs">
-                            <span itemprop="itemListElement">
-                                <a href="" itemprop="item" class="home">
-                                    <span itemprop="name">עמוד הבית</span>
-                                </a>
-                            </span>
-                            <span class="sep">|</span>
-                            <span class="current">תאורה</span>
-                            <span class="sep">|</span>
-                            <span class="current">תאורה</span>
-                        </div>
+                        <p> © 2018  כל הזכויות שמורות לחברת טכנולייט</p>
                     </div>
                 </div>
 
                 <div class="col-md-4 col-sm-6">
                     <div class="footer-column">
-                        <p>קידום אתרים בגוגל</p>
+                        <ul>
+                            <li><a href="">תאורת פנים</a></li>
+                            <li><a href="">תאורת פנים</a></li>
+                            <li><a href="">תאורת פנים</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="footer-column">
+                        <a href="#" target="_blank">
+                            <span>קידום אתרים בגוגל</span>
+                        </a>
                         <img src="img/digi_logo.png" alt="Digi" class="logo-bottom_line">
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</footer>
