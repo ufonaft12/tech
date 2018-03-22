c72c4aadf8ce89f379138f62d650812de49b3359
Gavrilovich Alexander <gavrilovich.a@gmail.com>
2018-03-22 16:22:24 +0200

    downloads.php

---
+++ downloads.php
@@ -0,0 +1,148 @@
+<?php include 'header.php';
+get_header("front-page"); ?>
+</div>
+
+<section class="section-download">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="breadcrumbs">
+                    <span class="current">בית</span>
+                    <span class="sep">/</span>
+                    <span itemprop="itemListElement">
+                        <a href="" itemprop="item" class="home">
+                            <span itemprop="name">בלוג</span>
+                        </a>
+                    </span>
+                </div>
+
+                <div class="download-item-head">
+                    <h1>הורדות</h1>
+                </div>
+                <div class="download-item-wrap">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="download-item">
+                                <div class="download-item-img" style="background-image: url('img/catalog1.jpg')"></div>
+
+                                <div class="download-item-content">
+                                    <h3>קטלוג מוצרי טריו</h3>
+                                    <ul>
+                                        <li><a href="">הורד קטלוג</a></li>
+                                        <li><a href="">לצפייה</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<link rel='stylesheet' id='main-style-css'  href='/css/downloads.min.css' type='text/css' media='all' />
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
