800e4fb5c57ea1d4135003c84d63b202850ad4ba
Gavrilovich Alexander <gavrilovich.a@gmail.com>
2018-03-22 14:28:34 +0200

    EDIT footer & blog

--- blog.php
+++ blog.php
@@ -1,36 +1,6 @@
-<!DOCTYPE html>
-<html dir="rtl" lang="he-IL">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
-    <title>GEO - front page</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <!-- Custom styles for this template -->
-    <!--    <link rel="stylesheet" href="css/owl.carousel.min.css">-->
-    <!--    <link rel="stylesheet" href="css/owl.theme.default.min.css">-->
-    <link rel="stylesheet" href="css/linearicons-free.min.css">
-    <link rel="stylesheet" href="css/opensanshebrew.min.css">
-    <link rel="stylesheet" href="css/fontello.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/footer.min.css">
-    <link rel="stylesheet" href="css/blog.min.css">
-    <link rel="stylesheet" href="css/front-page.css">
-    <link rel="stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-    <!--    <link rel="stylesheet" href="css/front-page-mobile.css">-->
-    <!--    <link rel ="stylesheet" href = "css/mobile.css">-->
-</head>
-
-<body>
 <?php include 'header.php';
 get_header("front-page"); ?>
-
+</div>
 <section class="section-blog">
     <div class="container">
         <div class="row">
@@ -327,7 +297,12 @@ get_header("front-page"); ?>
                                 <li><a href="">3</a></li>
                                 <li>...</li>
                                 <li><a href="">9</a></li>
-                                <li class="pagination-btn"><a href=""><span>הבא</span></a></li>
+                                <li class="pagination-btn">
+                                    <a href="">
+                                        <span class="btn-text">הבא</span>
+                                        <span class="lnr lnr-arrow-left"></span>
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -336,6 +311,8 @@ get_header("front-page"); ?>
         </div>
     </div>
 </section>
+
+<link rel='stylesheet' id='main-style-css'  href='/css/blog.min.css' type='text/css' media='all' />
 
 <?php include 'footer.php'; ?>
 </body>
