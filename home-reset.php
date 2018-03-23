9e8a1c1d57409c1c8ea5944708189080341cb05d
Gavrilovich Alexander <gavrilovich.a@gmail.com>
2018-03-23 12:49:23 +0200

    end home-reset.php

---
+++ home-reset.php
@@ -0,0 +1,37 @@
+<?php require_once ('header-about.php'); ?>
+<link href = "css/about.css" rel = "stylesheet">
+
+</div>
+<section class="section-reset" style="background-image: url('img/banner_login.jpg')">
+    <div class="section-flex">
+        <div class="flex-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="reset-content">
+                            <h1>שחזור סיסמה</h1>
+                            <p>הקלד את כתובת המייל ולינק ליצירת סיסמה ישלח אליך</p>
+
+                            <form action="" class="reset-form">
+                                <lable>
+                                    <span>דואר אלקטרוני</span>
+                                    <input type="text" name="text">
+                                </lable>
+
+                                <button class="button btn-form">שחזר סיסמה</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<link rel='stylesheet' id='main-style-css'  href='/css/home-reset.min.css' type='text/css' media='all' />
+
+<?php require_once ('footer.php')?>
+</body>
+
+</html>
+
