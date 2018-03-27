73450ead3acab9d398f24aea71208595290e062b
Singularity112 <singularity123097@gmail.com>
2018-03-27 23:04:01 +0300

    Add files via upload

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,43 +1,8 @@
-<<<<<<< HEAD
-var topline = document.getElementById('topline');
-var desc = document.getElementById('desctop-opener');
-var navigation = document.getElementById('menu');
-var headerLogo = document.getElementById('header-logo');
-var descOp = document.getElementById('desc-op');
-=======
->>>>>>> f1cfddc072d557c600d553041a0a5c9489e9c935
 
 
 jQuery('document').ready(function($){
    $('#mob-op').on('click', function(){
        $('#menu').toggleClass('mobile-open');
-<<<<<<< HEAD
-   });
-});
-
-var first = true;
-var nav = document.getElementById('menu');
-window.onscroll = function(){
-    var top = (window.pageYOffset || document.scrollTop)  - (document.clientTop || 0);
-    if (top > 50){
-        nav.classList.add('background');
-        if (first){
-            nav.classList.add('open');
-            first = false;
-        }
-        topline.classList.add('background');
-        headerLogo.classList.add('open');
-        desc.classList.add('open');
-        nav.classList.add('opacity');
-        nav.classList.remove('visible');
-    } else {
-        nav.classList.remove('background');
-        topline.classList.remove('background');
-        headerLogo.classList.remove('open');
-        desc.classList.remove('open');
-        nav.classList.remove('opacity');
-        nav.classList.add('visible');
-=======
 
        if ($('#menu').hasClass('mobile-open')){
            $(this).attr('src', 'img/close-light.png' );
@@ -60,6 +25,9 @@ window.onscroll = function(){
             navigation.classList.add('open');
             descOp.src = "img/hamburger-light.png";
         }
+
+        $('#header-logo').toggleClass('disp');
+        $('#desctop-opener').toggleClass('ml-auto');
     });
 
     var first = true;
@@ -85,7 +53,6 @@ window.onscroll = function(){
             nav.classList.remove('opacity');
             nav.classList.add('visible');
         }
->>>>>>> f1cfddc072d557c600d553041a0a5c9489e9c935
     }
 
 });
---
+++ js/myprojects.js
@@ -0,0 +1,49 @@
+jQuery('document').ready(function($){
+        var img = "";
+        $('.myproject-item-icons img').mouseenter(function(){
+            img = $(this).attr('src');
+            $(this).attr('src', $(this).attr('data-src'));
+        });
+
+        $('.myproject-item-icons img').mouseleave(function(){
+            $(this).attr('src', img);
+        });
+
+        $('.modal-body-close-dg').on('click', function(){
+       $('.modal-dg').fadeOut('300');
+    });
+
+    $('.create-new').on('click', function(){
+       $('#new-project').css({'display': 'flex'}).fadeIn('300');
+    });
+
+    $('.modal-button').on('click', function(){
+       if ($(this).hasClass('active')){
+           return;
+       }
+
+       $('.modal-dg').fadeOut('300');
+    });
+
+    $('.myproject-item-icon[data-tooltip = "שלח במייל"]').on('click', function(){
+        $('#send-mail').css({'display': 'flex'}).fadeIn('300');
+    });
+
+    $('.myproject-item-icon[data-tooltip = "מחק פרוייקט"]').on('click', function(){
+        $('#delete').css({'display': 'flex'}).fadeIn('300');
+    });
+
+    $('.change-pass').on('click', function(){
+       $('#change-pass').css({'display': 'flex'}).fadeIn(300);
+    });
+
+     $('.logout').on('click', function(){
+       $('#logout').css({'display': 'flex'}).fadeIn(300);
+    });
+
+     $('.delete').on('click', function(){
+       $('#delete').css({'display': 'flex'}).fadeIn(300);
+    });
+
+});
+
